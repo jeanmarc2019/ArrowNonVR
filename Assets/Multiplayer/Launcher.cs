484ddda70e6c133ad6c046896299c18ebe536acf
jeanmarc2019 <jmdp1337@gmail.com>
2020-11-18 18:43:50 -0500

    find room menu

--- Assets/Multiplayer/Launcher.cs
+++ Assets/Multiplayer/Launcher.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 using TMPro;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    public static Launcher Instance;
 
     [SerializeField] TMP_InputField roomNameInputField;
     [SerializeField] TMP_Text errorText;
@@ -13,6 +15,11 @@ public class Launcher : MonoBehaviourPunCallbacks
     [SerializeField] Transform roomListContent;
     [SerializeField] GameObject roomListItemPrefab;
     // Start is called before the first frame update
+
+    void Awake(){
+        Instance = this;
+    }
+
     void Start()
     {
         // tutorial set the loading screen to open explicitly, but this is probably cleaner
@@ -42,10 +49,6 @@ public class Launcher : MonoBehaviourPunCallbacks
     }
 
     public void CreateRoom() {
-        // empty strings create random room names, which we can do if we wanted
-//        if(string.IsNullOrEmpty(roomNameInputField.text)) {
-//            return;
-//        }
         PhotonNetwork.CreateRoom(roomNameInputField.text);
         MenuManager.Instance.OpenMenu("loading");
     }
@@ -64,13 +67,25 @@ public class Launcher : MonoBehaviourPunCallbacks
         PhotonNetwork.LeaveRoom();
         MenuManager.Instance.OpenMenu("loading");
     }
+
+    public void JoinRoom(RoomInfo info) {
+        PhotonNetwork.JoinRoom(info.Name);
+        MenuManager.Instance.OpenMenu("loading");
+    }
 //    //this probably isn't needed as OnConnectedToMaster() is called next by default, but if we wanted to do something here, we can
 //    public override void OnLeftRoom() {
 //        MenuManager.Instance.OpenMenu("title");
 //    }
 //
-//    public override void OnRoomListUpdate(List<RoomInfo> roomList) {
-//    }
+    public override void OnRoomListUpdate(List<RoomInfo> roomList) {
+        foreach(Transform trans in roomListContent) {
+            Destroy(trans.gameObject);
+        }
+        for(int i = 0; i < roomList.Count; i++) {
+            Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItemScript>().SetUp(roomList[i]);
+
+        }
+    }
 
     // Update is called once per frame
     void Update()
---
+++ Assets/RoomListItemScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Photon.Realtime;
+using TMPro;
+
+public class RoomListItemScript : MonoBehaviour
+{
+    [SerializeField] TMP_Text text;
+    RoomInfo info;
+
+    public void SetUp(RoomInfo _info) {
+        info = _info;
+        text.text = _info.Name;
+    }
+
+    public void OnClick() {
+        Launcher.Instance.JoinRoom(info);
+    }
+}
